931abdd54cf1c877db3f749232f12774e5edb50c
Syed Ali <sydali3103@gmail.com>
2024-02-10 04:26:53 +0530

    updated pages

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -1,8 +1,6 @@
 const navLinks = [
   { to: "/", title: "Home" },
   { to: "/plans", title: "Plans" },
-  { to: "/about", title: "About" },
-  { to: "/contact", title: "Contact" },
 ];
 
 export default navLinks;
